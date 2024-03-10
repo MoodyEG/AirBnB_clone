afc22d6d1d4b5ccc1d6de886de8b8514eccab9b7
MoodyEG <moodyeg2011@yahoo.com>
2024-03-10 10:57:46 -0700

    done fast

--- console.py
+++ console.py
@@ -85,6 +85,40 @@ class HBNBCommand(cmd.Cmd):
                     ins_list.append(value.__str__())
             print(ins_list)
 
+    def do_update(self, line):
+        """ Updates an instance based on the class name
+        and id by adding or updating attribute
+        Usage: vvv
+        update <class name> <id> <attribute name> "<attribute value>" """
+        if not line:
+            print("** class name missing **")
+            return
+        args = line.split(" ")
+        if args[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif len(args) == 2:
+            print("** attribute name missing **")
+        elif len(args) == 3:
+            print("** value missing **")
+        else:
+            data = storage.all()
+            for key, value in data.items():
+                ins_name = value.__class__.__name__
+                ins_id = value.id
+                if (ins_name == args[0]) and (ins_id == args[1]):
+                    if args[3][-1] != '"':
+                        for i in range(4, len(line)):
+                            args[3] += " " + args[i]
+                            if args[i][-1] == '"':
+                                break
+                    args[3] = args[3].replace('"', "")
+                    setattr(value, args[2], args[3])
+                    storage.save()
+                    return
+            print("** no instance found **")
+
     def do_quit(self, line):
         """ Quit command """
         return True
